ea688673f53e39c63769e9691b88f52eb5927afe
ahmaddeveloper17 <ahmaddeveloper17@gmail.com>
2024-01-23 22:36:37 +0500

    first commit

---
+++ src/app/store.tsx
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import currencyReducer from '../features/CurrencySlice';
+
+const store = configureStore({
+  reducer: {
+    currency: currencyReducer,
+    
+  },
+  
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
